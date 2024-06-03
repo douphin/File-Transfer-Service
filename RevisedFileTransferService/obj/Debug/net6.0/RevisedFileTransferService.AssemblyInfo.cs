08e4c07a0eb4a59e7783cedd3ba3fe2e0d573d1c
Doug Phinney <doug.phinney@clevelandcliffs.com>
2024-06-03 11:29:18 -0400

    Updated Dependencies

--- RevisedFileTransferService/obj/Debug/net6.0/RevisedFileTransferService.AssemblyInfo.cs
+++ RevisedFileTransferService/obj/Debug/net6.0/RevisedFileTransferService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+430e9532c1a5d6fd18a74684cec337e626e2bfb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f14ec4c443286ccbd9dd88d89543e414eb01a582
Doug Phinney <doug.phinney@clevelandcliffs.com>
2024-06-06 11:46:10 -0400

    Updated FlowCharts

--- RevisedFileTransferService/obj/Debug/net6.0/RevisedFileTransferService.AssemblyInfo.cs
+++ RevisedFileTransferService/obj/Debug/net6.0/RevisedFileTransferService.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e41754cdb6bfa893aeda6336add3da57351ec091")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RevisedFileTransferService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
